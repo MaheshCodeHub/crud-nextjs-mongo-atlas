8f1c356814dfe7927ced58f053aec782c35a4ad3
mahesh more <moremahesh3099@gmail.com>
2024-07-02 16:42:30 +0530

    Add new components and API routes, update layout and package configurations

---
+++ compoenents/AddUser.jsx
@@ -0,0 +1,91 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+
+function AddUser () {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const router=useRouter()
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if((!email || !password)){
+      alert("Email and Password are requried")
+      return;
+    }
+
+    try {
+      const res= await fetch("http://localhost:3000/api/user",{
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (res.ok) {
+        router.refresh()
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+  
+  return (
+    <div className="container h-screen my-10">
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        <div className="relative z-0 w-full mb-5 group ">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e)=>setEmail(e.target.value)}
+            value={email}
+            className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=""
+            required
+          />
+          <label
+            for="floating_email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Email address
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e)=>setPassword(e.target.value)}
+            className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=""
+
+            required
+          />
+          <label
+            for="floating_password"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Password
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddUser;
